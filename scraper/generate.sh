16a8a6ea0650c4922779bdfd3cba9d74f425d261
Ken Domino <kenneth.domino@domemtech.com>
2021-12-13 08:38:51 -0500

    Update "generate" script to output what the hell it is working on.

--- scraper/generate.sh
+++ scraper/generate.sh
@@ -1,4 +1,8 @@
-
+echo Making c++14.g4
 ./bin/Debug/net5.0/scrape-pdf.exe 'c:\Users\Kenne\Downloads\C++14-ISOIEC-148822014.pdf' > c++14.g4
+
+echo Making c++17.g4
 ./bin/Debug/net5.0/scrape-pdf.exe 'c:\Users\Kenne\Downloads\C++17-ISOIEC-148822017.pdf' > c++17.g4
+
+echo Making c++20.g4
 ./bin/Debug/net5.0/scrape-pdf.exe 'c:\Users\Kenne\Downloads\C++20-ISOIEC-148822020.pdf' > c++20.g4
